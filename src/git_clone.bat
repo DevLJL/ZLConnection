6f5120bec7a9cc6cb7cf963b06fccddacc62b10e
DevLJL <leonamjlima88@outlook.com>
2023-04-02 11:49:24 -0300

    alteração do gitclone

--- src/git_clone.bat
+++ src/git_clone.bat
@@ -2,13 +2,10 @@
 
 set vendor_path=%cd%\Vendor
 
-if exist %vendor_path% (
-  echo Deletando a pasta Vendor existente...
-  rmdir /s /q %vendor_path%
+if not exist %vendor_path% (
+  echo Criando a pasta Vendor em branco...
+  mkdir %vendor_path%
 )
-
-echo Criando a pasta Vendor em branco...
-mkdir %vendor_path%
 
 echo Clonando o repositório do Git dentro da pasta Vendor...
 git clone https://github.com/viniciussanchez/dataset-serialize.git %vendor_path%\DataSetSerialize
